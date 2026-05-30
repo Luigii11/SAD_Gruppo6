d0b131a89733fdda12f4bc0df7e01be591290ffe
Luigii_11 <luigi11autorino@gmail.com>
2026-05-30 17:21:43 +0200

    Base Classe Playlist + Rimozione file module-info.java

--- vibeflow/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module it.unisa.diem.sad_gruppo6 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens it.unisa.diem.sad_gruppo6 to javafx.fxml;
-    exports it.unisa.diem.sad_gruppo6;
-}
