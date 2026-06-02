dd481ecf7550c49b7eab022ccc3a719989cbd4fa
Emanuel Chirico <emanuelchirico03@gmail.com>
2026-06-02 11:56:32 +0200

    Modificato App.java con la prima schermata caricata per facilitare i test

--- vibeflow/src/main/java/it/unisa/diem/sad_gruppo6/App.java
+++ vibeflow/src/main/java/it/unisa/diem/sad_gruppo6/App.java
@@ -1,25 +1,33 @@
 package it.unisa.diem.sad_gruppo6;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class App extends Application {
 
+    private static Scene scene;
+
     @Override
-    public void start(Stage stage) {
-        // Schermata provvisoria per non far crashare l'app all'avvio
-        Label placeholder = new Label("VibeFlow - In attesa della HomeView...");
-        StackPane root = new StackPane(placeholder);
-        
-        Scene scene = new Scene(root, 640, 480);
-        
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("TrackLibraryView"), 720, 560);
         stage.setTitle("VibeFlow");
         stage.setScene(scene);
         stage.show();
     }
+
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("views/" + fxml + ".fxml"));
+    return fxmlLoader.load();
+}
 
     public static void main(String[] args) {
         launch();
