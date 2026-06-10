7f0303fe04e21002fac30d7c26b0003949e7bd38
Emanuela3009 <graziusoemanuela@gmail.com>
2026-06-10 11:23:26 +0200

    Implementazione classe di test LoopIteratorTest e aggiornamenti percorsi di Track nelle restanti classi di test

--- vibeflow/src/test/java/it/unisa/diem/sad_gruppo6/controllers/TrackTest.java
+++ vibeflow/src/test/java/it/unisa/diem/sad_gruppo6/controllers/TrackTest.java
@@ -6,11 +6,15 @@ import org.junit.jupiter.api.Test;
 
 import it.unisa.diem.sad_gruppo6.model.domain.Track;
 
+import java.io.File;
+
 public class TrackTest {
 
     @Test
-    public void testValidTrackCreation() {
-        Track track = new Track("Bohemian Rhapsody", "Queen", 354, "Rock", 1975, null);
+    public void testValidTrackCreation() throws Exception {
+        File f = File.createTempFile("track1_", ".mp3");
+        f.deleteOnExit();
+        Track track = new Track("Bohemian Rhapsody", "Queen", 354, "Rock", 1975, f.getAbsolutePath());
         assertEquals("Bohemian Rhapsody", track.getTitle());
         assertEquals(354, track.getDuration());
         assertEquals(1975, track.getYear());
@@ -29,15 +33,19 @@ public class TrackTest {
     }
 
     @Test
-    public void testNegativeDurationThrows() {
+    public void testNegativeDurationThrows() throws Exception {
         assertThrows(IllegalArgumentException.class, () ->
             new Track("Title", "Author", -5, "Pop", 2000, null));
     }
 
     @Test
-    public void testEqualsSameTitleAndAuthor() {
-        Track t1 = new Track("Title", "Author", 200, "Pop", 2000, null);
-        Track t2 = new Track("Title", "Author", 999, "Jazz", 1990, null);
+    public void testEqualsSameTitleAndAuthor() throws Exception{
+        File f1 = File.createTempFile("track1_", ".mp3"); 
+        f1.deleteOnExit();
+        File f2 = File.createTempFile("track2_", ".mp3"); 
+        f2.deleteOnExit();
+        Track t1 = new Track("Title", "Author", 200, "Pop", 2000, f1.getAbsolutePath());
+        Track t2 = new Track("Title", "Author", 999, "Jazz", 1990, f2.getAbsolutePath());
         assertEquals(t1, t2);   // stesso titolo+autore → stessa traccia
         assertEquals(t1.hashCode(), t2.hashCode());
     }
